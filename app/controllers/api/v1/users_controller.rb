ffb0edc8c82a2ef8f26ec97de386dfe0b1f7043e
David Ilizarov <dilizarov@gmail.com>
2015-08-30 06:00:23 -0500

    Of course, it helps if you make hashes properly and not with Swift syntax..

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApiController
   
   def upload_profile_pic
     render status: :ok,
-    json: { "user_id" : params[:user_id], "auth_token" : params[:auth_token] }
+    json: { user_id: params[:user_id], auth_token: params[:auth_token] }
   end
   
 end
