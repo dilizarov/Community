b4e2d780378eb22ab0b43d70d378cc0c3a577825
David Ilizarov <dilizarov@gmail.com>
2015-10-05 18:23:42 -0500

    Fixed accidental edit I made to logging in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::SessionsController < ApiController
   skip_before_filter :ensure_current_user!, only: [:create, :generate_meta_account]
   
   def create
-    @user = User.find_by!(email: params[:user][:email])
+    @user = User.find_by(email: params[:user][:email])
     
-    if @user.valid_password?(params[:user][:password])
+    if @user && @user.valid_password?(params[:user][:password])
       @user.login!
       
       render status: 200,
