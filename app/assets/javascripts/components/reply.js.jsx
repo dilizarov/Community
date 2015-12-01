9e06491460d8751840feff7b9c8d55856040fd18
David Ilizarov <dilizarov@gmail.com>
2015-11-30 19:16:57 -0600

    fixed reply like heart margin

--- app/assets/javascripts/components/reply.js.jsx
+++ app/assets/javascripts/components/reply.js.jsx
@@ -31,7 +31,7 @@ var Reply = React.createClass({
         <Avatar source={this.props.reply.user.avatar_url} />
         <span className="reply-username">{this.props.reply.user.username}</span>
         <div className="reply-body">{this.props.reply.body}</div>
-        <span className="reply-likes" onClick={this.likeReply}><i className={heartLikesClass}></i> {this.props.reply.likes.toThousandsString()}</span>
+        <span className="reply-likes" onClick={this.likeReply}><i className={heartLikesClass}></i>{this.props.reply.likes.toThousandsString()}</span>
         <span className="reply-timestamp">{timestamp(this.props.reply.created_at)}</span>
       </li>
     );
