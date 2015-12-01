5b208100871142452253b6f1e8d97fc2f25d4215
David Ilizarov <dilizarov@gmail.com>
2015-11-30 21:43:16 -0600

    made small version for avatar component. need to fix some reply stuff tho

--- app/assets/javascripts/components/avatar.js.jsx
+++ app/assets/javascripts/components/avatar.js.jsx
@@ -24,14 +24,22 @@ getInitialState: function() {
     var avatar;
     var loader;
 
+    var avatarClass = "avatar";
+    var loaderClass = "avatar-loading";
+
+    if (this.props.size === "sm") {
+      avatarClass += "-sm"
+      loaderClass += "-sm"
+    }
+
     if (this.props.source === null || this.state.loadFailed === true) {
-      avatar = <img src="/assets/avatar_placeholder.png" className="avatar" />
+      avatar = <img src="/assets/avatar_placeholder.png" className={avatarClass} />
     } else {
       if (this.state.loadSucceeded === true) {
-        avatar = <img src={this.props.source} className="avatar" />
+        avatar = <img src={this.props.source} className={avatarClass} />
       } else {
         avatar = <img src={this.props.source} style={{display: 'none'}} onLoad={this.makeVisible} onError={this.usePlaceholder} />
-        loader = (<span className="avatar-loading">
+        loader = (<span className={loaderClass}>
                     <span className="avatar-loading-rays"></span>
                   </span>)
       }
