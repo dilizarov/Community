a88136a767a2f7fc2236ab936f0d9cd8a9100856
David Ilizarov <dilizarov@gmail.com>
2016-02-02 00:43:26 -0600

    fixed text-align avatar rays loading issue and also have cache control working for browsers

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,9 @@ CarrierWave.configure do |config|
     aws_access_key_id: 'AKIAJGSLB2E7AK7X2T4Q',
     aws_secret_access_key: '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'
   }
-  
+
+
   config.fog_directory = "communitydev"
   config.fog_public = true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}"}
 end
