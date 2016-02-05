2f3be7068e990d8dbe922625829ca6b9f4abbf32
David Ilizarov <dilizarov@gmail.com>
2016-02-05 01:53:20 -0600

    got rid of delay for content animation for modals

--- app/assets/javascripts/components/drop_modal/drop_animation.js.jsx
+++ app/assets/javascripts/components/drop_modal/drop_animation.js.jsx
@@ -124,7 +124,6 @@
            opacity: 0,
            animationDuration: (willHidden ? hideAnimation : showAnimation).animationDuration,
            animationFillMode: 'forwards',
-           animationDelay: '0.25s',
            animationName: showContentAnimation,
            animationTimingFunction: (willHidden ? hideAnimation : showAnimation).animationTimingFunction
          })
