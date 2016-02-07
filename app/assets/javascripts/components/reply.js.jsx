bde2b07e18171063ebb9642fb0ccef0d39c796ee
David Ilizarov <dilizarov@gmail.com>
2016-02-07 17:24:41 -0600

    changed way space between reply username and reply body is handled

--- app/assets/javascripts/components/reply.js.jsx
+++ app/assets/javascripts/components/reply.js.jsx
@@ -30,7 +30,7 @@ var Reply = React.createClass({
       <li className="reply">
         <Avatar source={this.props.reply.user.avatar_url} size="sm" style={{float: 'left'}} />
         <div className="reply-data">
-          <span className="reply-username">{this.props.reply.user.username}</span>&nbsp;&nbsp;
+          <span className="reply-username">{this.props.reply.user.username}</span>
           <div className="reply-body">{this.props.reply.body}</div>
           <div className="reply-stats">
             <span className="reply-likes" onClick={this.likeReply}><i className={heartLikesClass}></i>{this.props.reply.likes.toThousandsString()}</span>
